522cbbe1653e4beadbf6a8bab9c7975a427779c9
sandy <sandeep.kumaraswamy@tyfone.com>
2022-11-08 13:10:21 +0530

    home-page-and-monthly-raking-update

--- resources/views/web/layout/includes/footer.blade.php
+++ resources/views/web/layout/includes/footer.blade.php
@@ -1,34 +1,41 @@
-
-<section class="footer_section pt-4">
+<section class="footer_section ">
     <div class="container">
-        <div class="row pt-5 pb-5">
+        <div class="row align-items-center ">
             <div class="col-md-4 col-lg-3">
                 <div class="footer_logo_sec">
                     <img src="{{asset('themes/frontend/assets/v1/img/logo-white.png')}}" alt='myplex-logo'/>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                    @if( isset( $optsDbArr['facebook'] ) || isset( $optsDbArr['twitter'] ) || isset( $optsDbArr['linkedin'] ) )
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+
+                    <div class="neswlatter_inner d-block d-sm-flex align-items-center mb-4">
+                        <div class="newsletter_input_group">
+                            <input type="text" placeholder="Enter Your Email" />
+                            <span><img src="{{asset('themes/frontend/assets/v1/img/newslatter_icon.png')}}" /></span>
+                        </div>
+                        <button class="subsribe_btn money_title_btn"><i class="ph-arrow-right-light"></i></button>
+                    </div>
+
                     <div class="footer_social">
+                        @if( isset( $optsDbArr['facebook'] ) || isset( $optsDbArr['twitter'] ) || isset( $optsDbArr['linkedin'] ) )
                         <ul class="d-flex align-items-center">
                             @if( isset( $optsDbArr['facebook'] ) )
-                                <li><a href="{{ $optsDbArr['facebook'] }}" target="_blank"><i class="ph-facebook-logo-light"></i></a></li>
-                            @endif
-                            @if( isset( $optsDbArr['twitter'] ) )
-                                <li><a href="{{$optsDbArr['twitter'] }}"><i class="ph-twitter-logo-light" target="_blank"></i></a></li>
-                            @endif
-                            @if( isset( $optsDbArr['linkedin'] ) )
-                            <li><a href="{{$optsDbArr['linkedin']}}"><i class="ph-instagram-logo-light" target="_blank"></i></a></li>
-                            @endif
+                            <li><a href="{{ $optsDbArr['facebook'] }}" target="_blank"><i class="ph-facebook-logo-light"></i></a></li>
+                        @endif
+                        @if( isset( $optsDbArr['twitter'] ) )
+                            <li><a href="{{$optsDbArr['twitter'] }}"><i class="ph-twitter-logo-light" target="_blank"></i></a></li>
+                        @endif
+                        @if( isset( $optsDbArr['linkedin'] ) )
+                        <li><a href="{{$optsDbArr['linkedin']}}"><i class="ph-instagram-logo-light" target="_blank"></i></a></li>
+                        @endif
                         </ul>
+                        @endif
                     </div>
-                    @endif
                 </div>
             </div>
             <div class="col-lg-9 col-md-8">
                 <div class="row">
                     <div class="col-md-6 col-lg-3 col-6 ">
-                        <div class="footer_menu">
+                        <div class="footer_menu color_inverse">
                             <h4>Important Links</h4>
-                            {{-- {!! $webFooterCompanyLinks!!} --}}
                             <ul>
                                 <li><a href="/">Home Page</a></li>
                                 <li><a href="/compare-scheme">Compare Scheme</a></li>
@@ -40,10 +47,9 @@
                     </div>
                     <div class="col-md-6 col-lg-3 col-6 ">
                         <div class="footer_menu">
-                            <h4>Navigation</h4>
-                            {{-- {!! $webFooterBusinessLinks !!} --}}
+                            <h4>Fund</h4>
                             <ul>
-                                    
+
                                 <li><a href="/fund-portfolio">Fund Portfolio</a></li>
                                 <li><a href="/fund-performance">Fund Performance</a></li>
                                 <li><a href="/fund-watch">Fund Watch</a></li>
@@ -53,9 +59,8 @@
                         </div>
                     </div>
                     <div class="col-md-6 col-lg-3 col-6 ">
-                        <div class="footer_menu">
+                        <div class="footer_menu color_inverse ">
                             <h4>Snapshot</h4>
-                            {{-- {!! $webFooterProductsLinks !!} --}}
                             <ul>
                                 <li><a href="/monthly-ranking">Rankings</a></li>
                                 <li><a href="/monthly-snapshot">Monthly Snapshot</a></li>
@@ -68,14 +73,13 @@
                     <div class="col-md-6 col-lg-3 col-6 ">
                         <div class="footer_menu">
                             <h4>Company</h4>
-                          {{-- {!!$webFooterExploreLinks!!} --}}
-                          <ul>
-                            <li><a href="/about">About Us</a></li>
+                            <ul>
+                                <li><a href="/about">About Us</a></li>
                             <li><a href="#">Contact Us</a></li>                                    
                             <li><a href="#">In The News</a></li>
                             <li><a href="/faq">FAQs</a></li>
                             <li><a href="/founder">Meet The Fund Man</a></li>
-                        </ul>
+                            </ul>
                         </div>
                     </div>
                 </div>
@@ -84,7 +88,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="copyright_inner d-sm-flex d-block align-items-center justify-content-between">
-                    <p>©Copyright 2022 All Rights Reserved.</p>
+                    <p>©Copyright {{date('Y')}} All Rights Reserved.</p>
                     <div class="copyright_menu">
                         <ul class="d-flex align-items-center justify-content-end">
                             <li><a href="#">Privacy Policy</a></li>
